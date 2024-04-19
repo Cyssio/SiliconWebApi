5840e828a8a10bf40b7b59df269190235721944a
Mattias <mattias.kasto@gmail.com>
2024-04-19 17:28:09 +0200

    added migration for courses and fixed my Get for courses

---
+++ Infrastructure/Entities/CourseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Entities;
+
+public class CourseEntity
+{
+    [Key]
+    public string Id { get; set; } = null!;
+    public string Image { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Price { get; set; } = null!;
+    public string? DiscountPrice { get; set; }
+    public string Hours { get; set; } = null!;
+    public string LikesInProcent { get; set; } = null!;
+    public string LikesInNumbers { get; set; } = null!;
+
+}
